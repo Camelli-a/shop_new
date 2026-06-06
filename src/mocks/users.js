219c060be6e0e800b2761a980f5fb4d088cd49e1
23301069 <23301069@bjtu.edu.cn>
2026-06-06 10:59:45 +0800

    Revert "feat(auth): login function"
    This reverts commit e88a9a1e88681811509771fb1027d56a0575b253.
    
    Reason for revert: revert
    
    Change-Id: If913f23476d21b6e5885e4ad430ba49da8c8c6c1

--- src/mocks/users.js
+++
@@ -1,8 +0,0 @@
-export const MOCK_USER = {
-  username: "admin",
-  password: "123456",
-  nickname: "京东用户",
-  phone: "138****8888",
-  gender: "男",
-  avatar: "/assets/home/icons/recommend.svg",
-};
